b8e618aa93e085e6086e526b05bf780c9c2ab36b
malathipraka <malathipriya.p@gmail.com>
2022-10-31 16:19:46 +0530

    sivagit

--- src/org/git3/Testng.java
+++ src/org/git3/Testng.java
@@ -1,5 +1,32 @@
 package org.git3;
 
+import org.testng.annotations.Test;
+
 public class Testng {
+	@Test
+	private void test1() {
+		System.out.println("testmethod test1");
+	}
+	@Test
+	private void test2() {
+		System.out.println("testmethod test2");
+	}
+	@Test
+	private void test8() {
+		System.out.println("testmethod test8");
+	}
+	   @Test
+	private void test9() {
+		System.out.println("testmethod test9");
+	}
+	   @Test
+	private void test10() {
+		System.out.println("testmethod test10");
+	}
+		
+	
+
+
+}
 
 }
